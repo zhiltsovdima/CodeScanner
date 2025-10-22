e91cec6475cec7e4cba10b54744cf0210b6d2625
zhiltsovdima <50846656+zhiltsovdima@users.noreply.github.com>
2025-10-22 09:55:00 +0300

    chore: project init

---
+++ CodeScanner/Supporting Files/CodeScannerApp.swift
@@ -0,0 +1,20 @@
+//
+//  CodeScannerApp.swift
+//  CodeScanner
+//
+//  Created by Dima Zhiltsov on 22.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct CodeScannerApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self)
+    var appDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
